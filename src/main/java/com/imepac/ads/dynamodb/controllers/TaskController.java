5d62e20674f7df4df1d3da47201ca4adb4740509
MaxCosta0 <maxleysoares006@gmail.com>
2024-09-26 17:23:59 -0300

    endpoint get

--- src/main/java/com/imepac/ads/dynamodb/controllers/TaskController.java
+++ src/main/java/com/imepac/ads/dynamodb/controllers/TaskController.java
@@ -3,11 +3,12 @@ package com.imepac.ads.dynamodb.controllers;
 import com.imepac.ads.dynamodb.entities.Tasks;
 import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -27,12 +28,28 @@ public class TaskController {
         return ResponseEntity.status(201).body("Task created successfully with ID: " + tasks.getTaskId());
     }
 
-//    @GetMapping("/{userId}/{taskId}")
-//    public ResponseEntity<Task> getTask(@PathVariable String userId, @PathVariable String taskId) {
-//        Task task = dynamoDbTemplate.ge(userId, taskId);
-//        if (task == null) {
-//            return ResponseEntity.status(404).body(null);
-//        }
-//        return ResponseEntity.ok(task);
-//    }
+    @GetMapping("/{userId}/{taskId}")
+    public ResponseEntity<List<Tasks>> getTask(@PathVariable String userId, @PathVariable String taskId) {
+
+        var key = Key.builder()
+                .partitionValue(userId)
+                .sortValue(taskId)
+                .build();
+
+        var condition = QueryConditional.sortBeginsWith(key);
+
+        var query = QueryEnhancedRequest.builder()
+                .queryConditional(condition)
+                .build();
+
+        try {
+            var task = dynamoDbTemplate.query(query, Tasks.class);
+
+            List<Tasks> tasks = task.items().stream().toList();
+            return ResponseEntity.ok(tasks);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
+    }
 }
